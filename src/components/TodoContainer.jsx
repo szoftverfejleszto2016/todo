44c2fa1baf09e078cde55b3dd1d2f8dd3e5fc829
infotanar <34937202+szoftverfejleszto2016@users.noreply.github.com>
2021-03-20 15:19:55 +0100

    Első lecke vége.

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
+import TodosList from './TodosList'
 
 function TodoContainer() {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+        },
+        {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+        },
+        {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+        }
+    ])
+
     return (
         <div>
-            
+            <Header />
+            <TodosList todos={todos} />
         </div>
     )
 }
---
+++ src/components/TodosList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+function TodosList(props) {
+    return (
+        <ul>
+            {props.todos.map(todo =>
+                <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                />
+            )}
+        </ul>
+    )
+}
+
+export default TodosList
